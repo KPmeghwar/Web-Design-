5000c49697cc9c509efe7aa2b973c346432130c4
Kailash Meghwar <premanikailash97@gmail.com>
2022-03-06 19:27:00 +0500

    Web Design Page

---
+++ Page_1.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>..::Web Page Design::..</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="color.css">
+</head>
+<body>
+<style type="text/css">
+	
+	
+</style>
+	<div class="row" >
+		<div class="col-12 bg-black">
+			<h3 style="color: white ;word-spacing: 10px;">Feedback  Login</h3>
+           	<h2 style="background: white;">Logo Here</h2>	
+		</div>
+	</div>
+	<div class="row" >
+		<div class="col-12 ">
+			<h2 align="center">Services</h2>	
+		</div>
+	</div>
+        
+    <div class="row">
+    	<div class="col-4 pro">
+				
+	</div>
+	
+	<div class="col-4 pro">
+				
+	</div>
+	
+	<div class="col-4 pro" >
+				
+	</div>
+    	
+    </div>
+	
+     <br/>
+	<div class="row" >
+		<div class="col-12 ">
+			<h2 align="center">Products</h2>	
+		</div>
+	</div>
+    
+    <div class="row">
+    	<div class="col-4 pro">
+				
+	</div>
+	
+	<div class="col-4 pro">
+				
+	</div>
+	
+	<div class="col-4 pro" >
+				
+	</div>
+    	
+    </div>
+     
+    <div class="row">
+	<div class="col-12 bg-black" style="margin:5px;">
+	<div class="col-4 ">
+	<h2 style="color: white ;text-align: center;">Company</h2>
+	<h3  style="color: white ;text-align: center;">Prdoduct</h3>
+	<h3  style="color: white;text-align: center;">Vendring</h3>
+	<h3  style="color: white;text-align: center;">Shipping</h3>	
+	</div>		
+
+	
+	<div class="col-4">
+	<h2  style="color: white; text-align:center;">Service</h2>
+	
+	<h3  style="color: white ;text-align: center;" >Prdoduct</h3>
+	<h3  style="color: white ;text-align: center;">Quality</h3>
+	<h3  style="color: white; text-align: center;">Shipping</h3>			
+	</div>	
+	<div class="col-4">
+	<h2  style="color: white ;text-align: center;">Contact</h2>
+	<h3  style="color: white ; text-align: center;">Address Here</h3>
+	<h3  style="color: white; text-align: center;">Phone No</h3>
+	<h3  style="color: white;text-align: center;">Facebook Link</h3>			
+	</div>
+	
+	</div>
+</body>
+</html>
